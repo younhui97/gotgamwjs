208272fad25d8840406766cf3d52c610d9db7505
yh <ssalborry@gmail.com>
2017-06-26 03:49:38 +0900

    170626 3:49

--- app/src/main/java/com/example/yh/gotgam/AlarmRing.java
+++ app/src/main/java/com/example/yh/gotgam/AlarmRing.java
@@ -1,20 +1,18 @@
 package com.example.yh.gotgam;
 
 import android.app.Activity;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 
-/**
- * Created by yh on 2017. 6. 26..
- */
+public class AlarmRing extends Activity {
+    private static MediaPlayer mp;
 
-public class AlarmRing extends Activity{
-
-        @Override
-        protected void onCreate(Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
-            setContentView(R.layout.alamring);
-
-        }
-
-
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.alamring);
+        mp = MediaPlayer.create(this, R.raw.sou);
+        mp.start();
+    }
 }
+
