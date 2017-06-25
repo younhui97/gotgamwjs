e8ef11b77811ec5568a62ef14a92595fe74c6728
yh <ssalborry@gmail.com>
2017-06-25 13:59:52 +0900

    170625 1:59

--- app/src/main/java/com/example/yh/gotgam/NextActivity.java
+++ app/src/main/java/com/example/yh/gotgam/NextActivity.java
@@ -1,30 +1,117 @@
 package com.example.yh.gotgam;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.TextView;
+import android.widget.ToggleButton;
 
-/**
- * Created by yh on 2017. 6. 9..
- */
 
 public class NextActivity extends Activity {
+
+    TextView tv;
+    ToggleButton tb;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_next);
+
+        tv = (TextView) findViewById(R.id.textView2);
+        tv.setText("위치정보 미수신중");
+
+        tb = (ToggleButton) findViewById(R.id.toggle1);
+
+        // LocationManager 객체를 얻어온다
+        final LocationManager lm = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+
+
+        tb.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    if (tb.isChecked()) {
+                        tv.setText("수신중..");
+                        // GPS 제공자의 정보가 바뀌면 콜백하도록 리스너 등록하기~!!!
+                        lm.requestLocationUpdates(LocationManager.GPS_PROVIDER, // 등록할 위치제공자
+                                100, // 통지사이의 최소 시간간격 (miliSecond)
+                                1, // 통지사이의 최소 변경거리 (m)
+                                mLocationListener);
+                        lm.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, // 등록할 위치제공자
+                                100, // 통지사이의 최소 시간간격 (miliSecond)
+                                1, // 통지사이의 최소 변경거리 (m)
+                                mLocationListener);
+                    } else {
+                        tv.setText("위치정보 미수신중");
+                        lm.removeUpdates(mLocationListener);  //  미수신할때는 반드시 자원해체를 해주어야 한다.
+                    }
+                } catch (SecurityException ex) {
+                }
+            }
+        });
+
+    } // end of onCreate
+
+
+    private final LocationListener mLocationListener = new LocationListener() {
+        public void onLocationChanged(Location location) {
+            //여기서 위치값이 갱신되면 이벤트가 발생한다.
+            //값은 Location 형태로 리턴되며 좌표 출력 방법은 다음과 같다.
+
+            Log.d("test", "onLocationChanged, location:" + location);
+            double longitude = location.getLongitude(); //경도
+            double latitude = location.getLatitude();   //위도
+            double altitude = location.getAltitude();   //고도
+            float accuracy = location.getAccuracy();    //정확도
+            String provider = location.getProvider();   //위치제공자
+            //Gps 위치제공자에 의한 위치변화. 오차범위가 좁다.
+            //Network 위치제공자에 의한 위치변화
+            //Network 위치는 Gps에 비해 정확도가 많이 떨어진다.
+            tv.setText("위치정보 : " + provider + "\n위도 : " + longitude + "\n경도 : " + latitude
+                    + "\n고도 : " + altitude + "\n정확도 : " + accuracy);
+
+
+
+        }
+
+        public void onProviderDisabled(String provider) {
+            // Disabled시
+            Log.d("test", "onProviderDisabled, provider:" + provider);
+        }
+
+        public void onProviderEnabled(String provider) {
+            // Enabled시
+            Log.d("test", "onProviderEnabled, provider:" + provider);
+        }
+
+        public void onStatusChanged(String provider, int status, Bundle extras) {
+            // 변경시
+            Log.d("test", "onStatusChanged, provider:" + provider + ", status:" + status + " ,Bundle:" + extras);
+        }
+
+
+    };
+    final LocationManager lm = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+    public void onClick02(View v) {
+        Location location1 = null;
+        double longitude = location1.getLongitude(); //경도
+        double latitude = location1.getLatitude();   //위도
+        double altitude = location1.getAltitude();   //고도
+        float accuracy = location1.getAccuracy();    //정확도
+        String provider = location1.getProvider();
+        Intent intent = new Intent(Intent.ACTION_SENDTO);
+        intent.putExtra("sms_body", " 지금 집 가는 중입니다. " + "경도" ) ;
+        intent.setData(Uri.parse("smsto:01087319379; 01087130031"));
+        startActivity(intent);
+
     }
-
-    public void onClick02(View v)
-    {
-        Intent intent = new Intent( Intent.ACTION_SENDTO );
-        intent.putExtra( "sms_body", " 지금 집 가는 중입니다. " );
-        intent.setData( Uri.parse( "smsto:01087319379; 01087130031" ) );
-        startActivity( intent );
-
-    }
-
 
 }
+
