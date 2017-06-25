dbcb4ea8392ebf7fef02a725c7d72df099e1bb38
yh <ssalborry@gmail.com>
2017-06-25 17:12:46 +0900

    170625 17:12

---
+++ app/src/main/java/com/example/yh/gotgam/NNextActivity.java
@@ -0,0 +1,17 @@
+package com.example.yh.gotgam;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by yh on 2017. 6. 25..
+ */
+
+public class NNextActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_nnext);
+
+    }
+}
